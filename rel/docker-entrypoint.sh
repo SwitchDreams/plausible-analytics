a8cfa16b0c588c08827d2fd97412cd5790a5e4df
João Pedro Assis <joaopedroassisdossantos@gmail.com>
2025-06-18 17:43:00 -0300

    entry

--- rel/docker-entrypoint.sh
+++ rel/docker-entrypoint.sh
@@ -2,7 +2,13 @@
 set -e
 
 if [ "$1" = 'run' ]; then
-  exec /app/bin/plausible start
-else
-  exec "$@"
+      exec /app/bin/plausible start
+
+elif [ "$1" = 'db' ]; then
+      exec /app/"$2".sh
+ else
+      exec "$@"
+
 fi
+
+exec "$@"
